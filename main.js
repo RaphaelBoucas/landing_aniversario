4cdd0d3122b42e179d72216b5c59f58d3483e1bb
Raphael Bouças <rafaladob@gmail.com>
2024-02-21 15:04:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const dataDoEvento = new Date("May 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    const segundosEmMs = 1000;
+
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor ((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor ((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor ((distanciaAteOEvento % minutoEmMs) / segundosEmMs);
+
+    
+    document.getElementById('contador__animado').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador__animado').innerHTML = `Evento Expirado`;
+    }
+
+}, 1000)
